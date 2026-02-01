856c59f6594e59acd9d56e7ec1f0fddb182840ed
I love licking the battery. <97060304+psicho100pro@users.noreply.github.com>
2026-02-01 10:47:58 +0100

    Stop and disable avahi-daemon in connection script
    Stop and disable the avahi-daemon service.

--- connection.sh
+++ connection.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
 sudo nmcli connection modify "Wired connection 1" \
     ipv4.addresses 192.168.50.50/24 \
     ipv4.gateway 192.168.50.1 \
     ipv4.method manual \
     ipv4.route-metric 100
 sudo nmcli connection up "Wired connection 1"
+
+sudo systemctl stop avahi-daemon
+sudo systemctl disable avahi-daemon
