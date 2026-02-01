85cb9d386ce73c8ecca93b01dc80e4e60be4ebab
I love licking the battery. <97060304+psicho100pro@users.noreply.github.com>
2026-02-01 10:40:50 +0100

    Add script to configure and activate network connection

---
+++ connection.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo nmcli connection modify "Wired connection 1" \
+    ipv4.addresses 192.168.50.50/24 \
+    ipv4.gateway 192.168.50.1 \
+    ipv4.method manual \
+    ipv4.route-metric 100
+sudo nmcli connection up "Wired connection 1"
